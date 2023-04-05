2d3a84afee58f69b9bc5fc249695d5f4f31aadb3
annisacut <129611768+annisacut@users.noreply.github.com>
2023-04-05 18:32:46 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+
+    include_once'controller.php';
+
+    $main_ctrl = new controller();
+    $main_ctrl ->__invoke();
+
+
+    ?>
---
+++ view.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title> MVC Dengan PHP </title>
+</head>
+<body>
+    <center>
+
+        <?=$data?>
+
+    </center>
+</body>
+</html>
